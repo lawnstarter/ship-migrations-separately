726e59991c89da583d3bbe745f6c3d23e98016e5
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2020-10-01 09:41:59 -0500

    v.0.0.1

---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function main() {
+  try {
+    const token = core.getInput("ghToken");
+    const githubClient = new github.GitHub(token);
+
+    const contextPayload = github.context.payload;
+
+    const endpointOptions = githubClient.pulls.listFiles.endpoint.merge({
+      owner: contextPayload.repository.owner.login,
+      repo: contextPayload.repository.name,
+      pull_number: contextPayload.pull_request.number,
+    });
+
+    let hasFileWithDropInName = false;
+
+    const files = await githubClient.paginate(endpointOptions, (response) =>
+      response.data.map((file) => {
+        if (file.filename.indexOf("drop_") !== -1) {
+          hasFileWithDropInName = true;
+        }
+        return file.filename;
+      })
+    );
+
+    let hasMoreThanOneFile = files.length > 1;
+
+    if (hasFileWithDropInName && hasMoreThanOneFile) {
+      core.setFailed("uh oh - you dropped something");
+    }
+
+    core.setOutput("files", JSON.stringify(files));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
