db370087b0ae57638ca02c5c969326ca48b27b1d
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2020-10-14 13:53:52 -0500

    008

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const github = require("@actions/github");
 async function main() {
   try {
     const token = core.getInput("token");
-    const pull_number = core.getInput("pull-number");
     const migration_directory = core.getInput("migration-directory");
     const githubClient = new github.GitHub(token);
 
-    const contextPayload = github.context.payload;
-
-    console.log("pull_number", pull_number);
+    const githubContext = github.context;
+    const pullRequest = githubContext.issue;
 
     const endpointOptions = githubClient.pulls.listFiles.endpoint.merge({
-      owner: contextPayload.repository.owner.login,
-      repo: contextPayload.repository.name,
-      pull_number,
+      owner: pullRequest.owner,
+      repo: pullRequest.repo,
+      pull_number: pullRequest.number,
     });
 
     let hasOneOrMoreMigration = false;
